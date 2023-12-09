1540dd8b8197cf08ce7edf239f9e5336f4684240
Lahlou ibrahim <ibrahimlahlou021@gmail.com>
2023-12-09 17:57:10 +0100

    TASK 7 : Data Ingestion Stage Completed with Github repo

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from WineX import logger
+from WineX.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Hello World!")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
