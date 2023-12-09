cb7b3388585cc2b45c8edf988846acc89d93c3b1
Lahlou ibrahim <ibrahimlahlou021@gmail.com>
2023-12-09 01:23:41 +0100

    TASK 6 : Data Ingestion Research Completed

--- src/WineX/constants/__init__.py
+++ src/WineX/constants/__init__.py
@@ -3,3 +3,4 @@ from pathlib import Path
 CONFIG_FILE_PATH = Path("config/config.yaml")
 PARAMS_FILE_PATH = Path("params.yaml")
 SCHEMA_FILE_PATH = Path("schema.yaml")
+CREDENTIALS_FILE_PATH = Path("credentials.json")
