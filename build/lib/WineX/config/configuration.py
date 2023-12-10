e0e168ff47266732968d2549e2aa439d60b5ec2a
Lahlou ibrahim <ibrahimlahlou021@gmail.com>
2023-12-10 02:33:30 +0100

    TASK 12 : Model Evaluation Completed using Mlflow

--- build/lib/WineX/config/configuration.py
+++ build/lib/WineX/config/configuration.py
@@ -4,7 +4,8 @@ from WineX.utils.common import read_yaml, create_directories
 from WineX.entity.config_entity import (DataIngestionConfig
                                         ,DataValidationConfig
                                         ,DataTransformationConfig
-                                        ,ModelTrainerConfig)
+                                        ,ModelTrainerConfig
+                                        ,ModelEvaluationConfig)
 
 
 # ConfigurationManager class for managing configurations
@@ -86,3 +87,23 @@ class ConfigurationManager:
         )
 
         return model_trainer_config
+    
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+        params = self.params.ElasticNet
+        schema =  self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            test_data_path=config.test_data_path,
+            model_path = config.model_path,
+            all_params=params,
+            metric_file_name = config.metric_file_name,
+            target_column = schema.name,
+            mlflow_uri="https://dagshub.com/IbLahlou/ElasticNet_model_e2e.mlflow",
+           
+        )
+
+        return model_evaluation_config
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from WineX.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from WineX.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from WineX.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from WineX.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
-
+from WineX.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -50,3 +50,12 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+STAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
