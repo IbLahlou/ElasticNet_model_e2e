234808ec15dd4df02c547e06c17d15202ffd6d60
Lahlou ibrahim <ibrahimlahlou021@gmail.com>
2023-12-13 01:18:45 +0100

    TASK 14 : Building a falsk app 1

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request
+import os 
+import numpy as np
+import pandas as pd
+from WineX.pipeline.prediction import PredictionPipeline
+import json
+
+app = Flask(__name__) # initializing a flask app
+
+@app.route('/',methods=['GET'])  # route to display the home page
+def homePage():
+    return render_template("index.html")
+
+
+@app.route('/train',methods=['GET'])  # route to train the pipeline
+def training():
+    os.system("python main.py")
+    with open('artifacts/model_evaluation/metrics.json', 'r') as file:
+        metrics_data = json.load(file)
+
+    return render_template('index.html',**metrics_data)
+
+
+@app.route('/predict',methods=['POST','GET']) # route to show the predictions in a web UI
+def index():
+    if request.method == 'POST':
+
+            #  reading the inputs given by the user
+            fixed_acidity =float(request.form['fixed_acidity'])
+            volatile_acidity =float(request.form['volatile_acidity'])
+            citric_acid =float(request.form['citric_acid'])
+            residual_sugar =float(request.form['residual_sugar'])
+            chlorides =float(request.form['chlorides'])
+            free_sulfur_dioxide =float(request.form['free_sulfur_dioxide'])
+            total_sulfur_dioxide =float(request.form['total_sulfur_dioxide'])
+            density =float(request.form['density'])
+            pH =float(request.form['pH'])
+            sulphates =float(request.form['sulphates'])
+            alcohol =float(request.form['alcohol'])
+       
+         
+            data = [fixed_acidity,volatile_acidity,citric_acid,residual_sugar,chlorides,free_sulfur_dioxide,total_sulfur_dioxide,density,pH,sulphates,alcohol]
+            data = np.array(data).reshape(1, 11)
+            
+            obj = PredictionPipeline()
+            predict = obj.predict(data)
+            with open('artifacts/model_evaluation/metrics.json', 'r') as file:
+                metrics_data = json.load(file)
+
+
+            return render_template('index.html', prediction = str(predict),**metrics_data)
+
+
+    else:
+        return render_template('index.html')
+
+
+if __name__ == "__main__":
+	# app.run(host="0.0.0.0", port = 8080, debug=True)
+	app.run(host="0.0.0.0", port = 8080)
